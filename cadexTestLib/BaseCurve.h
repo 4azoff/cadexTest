25203eb389dc8b89ac81d469ae5c64f2ea3f5560
4azoff <m.v.chazov@gmail.com>
2022-09-29 11:10:41 +0300

    Доработан класс базовой кривой

--- cadexTestLib/BaseCurve.h
+++ cadexTestLib/BaseCurve.h
@@ -1,18 +1,38 @@
 #pragma once
-#include "cadexTestLib.h"
+
+#ifndef _BASECURVE_H_
+#define _BASECURVE_H_
+
+#include "pch.h"
+#include "Utility.h"
+#include <iostream>
+
+using namespace cadexUtility;
 
 class CADEXTESTLIB_API BaseCurve {
-
 protected:
-	double tParam;	
+	double tParam;
+	virtual double GetXCoord() const = 0;
+	virtual double GetYCoord() const = 0;
+	virtual double GetZCoord() const = 0;
+	virtual void PrintInfoAboutType() = 0;
 
 public:
 	BaseCurve(const double& _tParam) : tParam{ _tParam } {};
-	virtual ~BaseCurve() = default;
-
-	virtual double GetXPoint() const = 0;
-	virtual double GetYPoint() const = 0;
-	virtual double GetZPoint() const = 0;
 	double GetParamT() const { return tParam; };
 	void SetParamT(const double& tParam) { this->tParam = tParam; };
+	Point3D Get3DPoint() const { return Point3D(GetXCoord(), GetYCoord(), GetZCoord()); };
+	virtual Vector3D GetFirstDerivativeVector() const = 0;
+
+	void PrintInfo()
+	{
+		auto point = this->Get3DPoint();
+		auto vector = this->GetFirstDerivativeVector();
+		std::cout << "==============================================\n";
+		this->PrintInfoAboutType();
+		std::cout << "3Dpoint:\nx:" << point.x << "\ny:" << point.y << "\nz:" << point.z
+			<< "\nFirstDerivativeVector:\nx:" << vector.x << "\ny:" << vector.y << "\nz:" << vector.z << "\n";		
+		std::cout << "==============================================\n";
+	};
 };
+#endif;
